4bff5a37acc5a7dca1c5d18177c172780c098725
omgohmygod <t105590013@ntut.org.tw>
2018-12-06 23:14:58 +0800

    pratice but no pic

---
+++ index.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+
+const router = Router();
+
+const bodyParser = require('koa-bodyparser');
+
+app.use(bodyParser());
+
+// Router -> /
+router.get('/', async(ctx) => {
+    ctx.body = `
+    <form method="POST" action="/">
+        <label>我有test1/2分頁哦</label><br/>
+        <label>輸入東西</label>
+        <input name="usr" /><br/>
+        <button type="submit">回傳給你看</button>
+      </form>
+    `;
+});
+
+router.post('/', async(ctx) => {
+    let usr = ctx.request.body.usr;
+    ctx.body = `<p>Welocome,${usr}!</p>`;
+});
+
+// Router -> /about
+router.get('/test1', async(ctx) => {
+    ctx.body = 'test1';
+});
+
+router.get('/test2', async(ctx) => {
+    ctx.body = 'test2';
+});
+
+app.use(router.routes());
+
+app.listen(3000);
